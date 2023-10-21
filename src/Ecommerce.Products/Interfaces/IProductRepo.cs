8688596d6e262ef2e820cce1d05389f1305e8df0
RRJos <RRJos@JARR>
2023-10-21 15:16:06 -0600

    SE-01-Agregar proyecto base

---
+++ src/Ecommerce.Products/Interfaces/IProductRepo.cs
@@ -0,0 +1,13 @@
+﻿using EcommerceKernel.Interfaces;
+
+namespace Ecommerce.Products.Interfaces;
+
+public interface IProductRepo : IBaseRepo
+{
+    /// <summary>
+    /// Agrega o actualiza un producto
+    /// </summary>
+    /// <param name="product"></param>
+    /// <returns></returns>
+    Task Update(List<Product> product);
+}
