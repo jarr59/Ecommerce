8068bd493bb024cf1915d922f6c3e5526b4b6296
RRJos <RRJos@JARR>
2023-10-21 15:16:37 -0600

    SE-01-Agregar proyecto data

---
+++ src/Ecommerce.Products.Data/Repositories/ProductRepo.cs
@@ -0,0 +1,24 @@
+﻿using Ecommerce.Products.Interfaces;
+
+namespace Ecommerce.Products.Data.Repositories
+{
+    public class ProductRepo : IProductRepo
+    {
+        readonly ProductContext _productContext;
+
+        public ProductRepo(ProductContext productContext)
+        {
+            _productContext = productContext;
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _productContext.SaveChangesAsync();
+        }
+
+        public async Task Update(List<Product> products)
+        {
+            await _productContext.Products.AddRangeAsync(products);
+        }
+    }
+}
